09e848c4d237689951d2789cf82495428aae4ee4
Yeongdo Jung <yeongdojung@Yeongdoui-MacBookPro.local>
2024-09-26 15:56:26 +0900

    modified:   .gitignore 	new file:   dist/logo.png 	new file:   ios/evryai-chat/evryai-chat.xcodeproj/project.pbxproj 	new file:   ios/evryai-chat/evryai-chat.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   ios/evryai-chat/evryai-chat.xcodeproj/project.xcworkspace/xcuserdata/yeongdojung.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   ios/evryai-chat/evryai-chat.xcodeproj/xcuserdata/yeongdojung.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   ios/evryai-chat/evryai-chat/Assets.xcassets/AccentColor.colorset/Contents.json 	new file:   ios/evryai-chat/evryai-chat/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   ios/evryai-chat/evryai-chat/Assets.xcassets/Contents.json 	new file:   ios/evryai-chat/evryai-chat/ContentView.swift 	new file:   ios/evryai-chat/evryai-chat/Preview Content/Preview Assets.xcassets/Contents.json 	new file:   ios/evryai-chat/evryai-chat/evryai_chat.entitlements 	new file:   ios/evryai-chat/evryai-chat/evryai_chatApp.swift 	modified:   package-lock.json 	modified:   package.json 	modified:   yarn.lock
     Changes not staged for commit:
    	modified:   evryai-chat-ydj-o9myigc4ofcpcpeo5sa8 (modified content)

---
+++ ios/evryai-chat/evryai-chat/evryai_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  evryai_chatApp.swift
+//  evryai-chat
+//
+//  Created by Yeongdo Jung on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct evryai_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
